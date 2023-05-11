31c2a7f912cf3aa54d887428d5b3212b073fe489
Mark <ajineer@outlook.com>
2023-05-11 14:49:20 -0600

    updated structure for toDo list and added styling

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
-const listOfItems = document.getElementById("listItems");
 const entBtn = document.getElementById("EnterItem");
 let userInput = document.getElementById('toDo');
 
-
-entBtn.addEventListener("click", ()=>{
+entBtn.addEventListener("click", (e)=>{
+    e.preventDefault();
 
     if(userInput.value !== ""){
-        let toDoItem = document.createElement('li');
-        let chkBox = document.createElement("input");
+        const listOfItems = document.getElementById("toDoList");
+        let checkBox = document.createElement('button');
+        let todoItem = document.createElement('li');
+        let trashButton = document.createElement('button');
+        let todoText = document.createElement('span');
         
-        chkBox.type = "checkbox"
-        toDoItem.innerText = userInput.value;
-        toDoItem.appendChild(chkBox);
-        listOfItems.appendChild(toDoItem);
+        todoText.innerText = userInput.value;
+         
+        trashButton.setAttribute("id", "trashButton");
+        trashButton.innerHTML = '<i>&#10006;</i>'
+        trashButton.addEventListener("click", ()=>{
+            todoItem.remove();
+        })
+        
+        checkBox.setAttribute("id", "checkBox");
+        checkBox.innerHTML = '<i>&check;</i>';
+        checkBox.addEventListener("click", ()=>{
+            checkBox.style.backgroundColor = "limegreen";
+        })
+        todoItem.appendChild(todoText);
+        todoItem.appendChild(checkBox);
+        todoItem.appendChild(trashButton);
+        listOfItems.appendChild(todoItem);
 
     }else{
         alert("You must enter something!");
     }
 })
+
+
+
+
 
 
 
