5141ce362aef2f1e4320df0b8d3a39f3590226df
Mark <ajineer@outlook.com>
2023-05-13 10:49:00 -0600

    redoing some styling

--- index.js
+++ index.js
@@ -1,46 +1,44 @@
 const entBtn = document.getElementById("EnterItem");
 let userInput = document.getElementById('toDo');
 let itemCount = 0;
-
-function createNewElement(tagName, idName, aClassName){
-    let newElm = document.createElement(tagName);
-    newElm.setAttribute("id", idName);
-    newElm.setAttribute("class", aClassName);
-    return newElm;
-}
+let charCount = userInput.value.length;
 
 entBtn.addEventListener("click", (e)=>{
 
+    function createNewElement(tagName, idName, aClassName){
+        let newElm = document.createElement(tagName);
+        newElm.setAttribute("id", idName);
+        newElm.setAttribute("class", aClassName);
+        return newElm;
+    }
+
     e.preventDefault();
 
-    if(userInput.value !== "" && itemCount <= 12){
-        const listOfItems = document.getElementById("toDoList");
-        //let checkBox = document.createElement('button');
-        let checkBox = createNewElement("button", "checKBox", "");
-        let todoItem = document.createElement('li');
-        let trashButton = document.createElement('button');
-        let todoText = document.createElement('span');
+    if(userInput.value !== "" && itemCount <= 12 && charCount <= 14){
+        const listOfItems = document.getElementById('toDoList');
+        let completeBox = createNewElement('button', 'checKBox', '');
+        let todoItem = createNewElement('li', "", "")
+        let trashButton = createNewElement('button', 'trashButton', '');
+        let todoText = createNewElement('p', '', '');
         
         todoText.innerText = userInput.value;
-         
-        trashButton.setAttribute("id", "trashButton");
         trashButton.innerHTML = '<i>&#10006;</i>'
+        completeBox.innerHTML = '<i>&check;</i>';
+        
+        todoItem.appendChild(todoText);
+        todoItem.appendChild(completeBox);
+        todoItem.appendChild(trashButton);
+        listOfItems.appendChild(todoItem);
+        itemCount++;
+
+        completeBox.addEventListener("click", ()=>{
+            completeBox.style.backgroundColor = "limegreen";
+        })
+
         trashButton.addEventListener("click", ()=>{
             todoItem.remove();
             itemCount--;
         })
-        
-        //checkBox.setAttribute("id", "checkBox");
-        checkBox.innerHTML = '<i>&check;</i>';
-        checkBox.addEventListener("click", ()=>{
-            checkBox.style.backgroundColor = "limegreen";
-        })
-        
-        todoItem.appendChild(todoText);
-        todoItem.appendChild(checkBox);
-        todoItem.appendChild(trashButton);
-        listOfItems.appendChild(todoItem);
-        itemCount++;
 
     }else{
         alert("You must enter something or their are too many items in list.");
