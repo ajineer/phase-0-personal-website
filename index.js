ac8999a75bc83fa6383641939dbce511f27d15c4
Mark <ajineer@outlook.com>
2023-05-13 14:41:47 -0600

    tweaked styling, refactored some more javascript

--- index.js
+++ index.js
@@ -3,23 +3,25 @@ let userInput = document.getElementById('toDo');
 let itemCount = 0;
 let charCount = userInput.value.length;
 
-entBtn.addEventListener("click", (e)=>{
+function createNewElement(tagName, idName, aClassName){
+    let newElm = document.createElement(tagName);
+    newElm.setAttribute('id', idName);
+    newElm.setAttribute('class', aClassName);
+    return newElm;
+}
 
-    function createNewElement(tagName, idName, aClassName){
-        let newElm = document.createElement(tagName);
-        newElm.setAttribute("id", idName);
-        newElm.setAttribute("class", aClassName);
-        return newElm;
-    }
+entBtn.addEventListener("click", (e)=>{
 
     e.preventDefault();
 
     if(userInput.value !== "" && itemCount <= 12 && charCount <= 14){
         const listOfItems = document.getElementById('toDoList');
-        let completeBox = createNewElement('button', 'checKBox', '');
+        let completeBox = createNewElement('button', 'checKBox', 'boxCheck');
+        console.log(completeBox.getAttribute('id'));
         let todoItem = createNewElement('li', "", "")
         let trashButton = createNewElement('button', 'trashButton', '');
         let todoText = createNewElement('p', '', '');
+
         
         todoText.innerText = userInput.value;
         trashButton.innerHTML = '<i>&#10006;</i>'
